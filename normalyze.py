e0127176f5ed67bf74c16ff4b22cf72edb8f699d
zviwex <zvikizviki@gmail.com>
2018-06-12 23:00:36 +0300

    Added module Support Added vscode configuration

--- normalyze.py
+++ normalyze.py
@@ -1,27 +1,25 @@
 import sys
 import os
 
-def indent(line, debth):
+def indent(line, depth):
     line = line.strip()
-    return ("    " * debth) + line
+    return ("    " * depth) + line
 
-def opening_statement(line, debth):
+def opening_statement(line, depth):
     if line[-1] != ":":
         line = line + ":"
-    return indent(line, debth)
+    return indent(line, depth)
 
-def branch(line, debth):
-    return indent(line[1:], debth)
+def branch(line, depth):
+    return indent(line[1:], depth)
 
 
 def _parse(code, is_module):
-    # TODO: check long string (""" val """)
-    debth = 0
+    depth = 0
     new_code = []
     for line_idx in xrange(len(code)):
         if is_module:
-            if "import normalyze" in code[line_idx] or "from normalyze" in code[line_idx] or "normalyze.run" in code[line_idx]:
-                continue
+            code[line_idx] = code[line_idx].replace('"-"-"', '"""')
         if len(code[line_idx]) > 0 and code[line_idx][0] == '{' and line_idx != 0:
             i = -1
             while new_code[i].strip() == "":
@@ -31,25 +29,25 @@ def _parse(code, is_module):
             while new_code[j].strip() != new_code[i].strip():
                 j -= 1
 
-            new_code[i] = opening_statement(new_code[j], debth)
-            line = branch(code[line_idx], debth)
+            new_code[i] = opening_statement(new_code[j], depth)
+            line = branch(code[line_idx], depth)
             if line.strip() != "":
                 new_code.append(line)
-            debth += 1
+            depth += 1
         elif len(code[line_idx]) > 0 and code[line_idx][0] == '}':
-            line = branch(code[line_idx], debth)
+            line = branch(code[line_idx], depth)
             if line.strip() != "":
                 new_code.append(line)
-            debth -= 1
+            depth -= 1
         else:
-            new_code.append(indent(code[line_idx], debth))
+            new_code.append(indent(code[line_idx], depth))
     return new_code
 
 def _load(codelist):
     code = "\n".join(codelist)
-    exec(code)
+    exec(code, globals())
 
-def parse_file_and_load(file_path, is_module = False):
+def parse_file_and_save(file_path, is_module = False):
     if ".npy" not in file_path[-4:]:
         raise(Exception("Error: not npy file"))
 
@@ -57,11 +55,13 @@ def parse_file_and_load(file_path, is_module = False):
         code = [x.strip() for x in f.read().splitlines()]
     
     code = _parse(code, is_module)
+    
+    with open(file_path[:-4] + ".py", 'w') as f:
+        f.write("\n".join(code))
+    
 
-    _load(code)
 
-
-def main(args):
+def as_runner(args):
     is_dir = False
     for arg in args:
         if '-d' == arg:
@@ -73,18 +73,19 @@ def main(args):
         for root, _, files in os.walk(path):
             for ffile in files:
                 file_path = os.path.join(root, ffile)
-                parse_file_and_load(file_path) 
+                parse_file_and_save(file_path) 
     else:
-        parse_file_and_load(path)
+        parse_file_and_save(path)
 
 
-def parse_importer_file(file_path):
-    parse_file_and_load(file_path, True)
 
-def run(file_path):
-    parse_importer_file(os.path.realpath(file_path))
+def run(code):
+    code = [x.strip() for x in code.splitlines()]
+    code = _parse(code, True)
 
-#if __name__ == "normalyze":
+    code = "\n".join(code)
+    return(code)
+
 
 if __name__ == "__main__":
-    main(sys.argv)
+    as_runner(sys.argv)
